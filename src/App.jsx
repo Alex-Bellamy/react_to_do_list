12e63c9a897a796f90f4844a217b35f00711b206
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-27 20:02:18 +0200

    adding functions to app.jsx to enable tasks to be stored to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./components/TodoList"
 import uuidv4 from 'uuid/v4'
+
+const LOCAL_STORAGE = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
   const taskNameRef = useRef()
+
+  useEffect (() => {
+  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE))
+  if (storedTodos) setTodos(storedTodos)
+}, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE, JSON.stringify (todos))
+  }, [todos])
 
   function handleAddTask(e) {
     const name = taskNameRef.current.value
