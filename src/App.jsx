6213ce65c2000c70761af97ce56f50bcdce8f313
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-27 08:19:29 +0200

    creating todolist component and html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoList"
 
 function App() {
   return (
-    <div>
-    </div>
-  );
+    <>
+    <TodoList />
+    <input type="text" />
+    <button>Add Task</button>
+    <button>Clear Completed Tasks</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
